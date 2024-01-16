aed1b579977abf6e8ebaee59d5979e802d252ad9
mrjohn7751 <jahongirberdiyev7751@gmail.com>
2024-01-16 14:16:00 +0500

    done

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,50 +13,7 @@
     </div>
     <div class="container">
       <div class="row card_description mx-0">
-        <!-- <div class="col col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6 col-xxl-6">
-            <div id="carouselExampleCaptions" class="carousel slide">
-                <div class="carousel-indicators">
-                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
-                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
-                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="5" aria-label="Slide 5"></button>
-                  <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="6" aria-label="Slide 5"></button>
-                </div>
-                <div class="carousel-inner">
-                  <div class="carousel-item active">
-                    <img src="./img/photo_1.jpg" class="d-block w-100 img-slide" alt="...">
-                  </div>
-                  <div class="carousel-item">
-                    <img src="./img/photo_2023-07-18_13-42-11.jpg" class="d-block w-100 img-slide" alt="...">
-                  </div>
-                  <div class="carousel-item">
-                    <img src="./img/photo_2023-07-18_13-42-24.jpg" class="d-block w-100 img-slide" alt="...">
-                  </div>
-                  <div class="carousel-item">
-                    <img src="./img/photo_2023-07-18_13-42-29.jpg" class="d-block w-100 img-slide" alt="...">
-                  </div>
-                  <div class="carousel-item">
-                    <img src="./img/photo_2023-07-18_13-42-33.jpg" class="d-block w-100 img-slide" alt="...">
-                  </div>
-                  <div class="carousel-item">
-                    <img src="./img/photo_2023-07-18_13-42-37.jpg" class="d-block w-100 img-slide" alt="...">
-                  </div>
-                  <div class="carousel-item">
-                    <img src="./img/photo_2023-07-18_13-42-41.jpg" class="d-block w-100 img-slide" alt="...">
-                  </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="visually-hidden">Next</span>
-                </button>
-              </div>
-        </div> -->
+
         <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
             <div class="content">
                 <h5>Jizzax politexnika instituti O'qishni ko'chirish va tiklash</h5>
